045ae0e59ab7e2ca024547edebf0bbd0c15e8e10
Fábia Emanuele Cardoso Sena <61851602+fabiacardoso@users.noreply.github.com>
2022-02-20 22:58:51 -0300

    Update Escada.java

--- desafioCap/src/desafioCap/Escada.java
+++ desafioCap/src/desafioCap/Escada.java
@@ -2,23 +2,28 @@ package desafioCap;
 import javax.swing.JOptionPane;
 
 /*
- * # Questão 01 - Escada
+ * # QuestÃ£o 01 - Escada
 
 * Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * 
-* e espaços. A base e altura da escada devem ser iguais ao valor de n. A última linha não 
-* deve conter nenhum espaço.
+* e espaÃ§os. A base e altura da escada devem ser iguais ao valor de n. A Ãºltima linha nÃ£o 
+* deve conter nenhum espaÃ§o.
 */
 
 public class Escada {
 
 	public static void main(String[] args) {
-			
-		int n = Integer.parseInt(JOptionPane.showInputDialog("Informe a altura da escada"));
 		
-		for (int i = 0; i < n; i++) {
-			String resultado = " ".repeat(n - i -1) + "*".repeat(i + 1);
-			System.out.println(resultado);
-		}
-	}
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Informe a altura da escada"));
+		StringBuilder escada = new StringBuilder();
 
+		
+        for (int i = 0; i < n; i++) {
+            escada.append("*");
+            if (i == 0)
+                System.out.print(escada);
+            else
+                System.out.print("\n" + escada);
+        }
+    }
 }
+
